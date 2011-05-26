f58b978b029a2346326bfb3a5bf8feaa69a80076
Fernando Santos <o@fernan.do>
2011-05-26 04:53:59 -0300

    Adicionado Bundles para Teste e Tema

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,10 +22,12 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 
             new Mandango\MandangoBundle\MandangoBundle(),
+            new Mastop\SystemBundle\MastopSystemBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            $bundles[] = new Mastop\TesteBundle\MastopTesteBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Symfony\Bundle\WebConfiguratorBundle\SymfonyWebConfiguratorBundle();
         }
