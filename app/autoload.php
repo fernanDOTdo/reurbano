bcbb1fd28cdb982f3c83c6d74ac8f14026fb2cd4
Fernando Santos <o@fernan.do>
2011-05-26 16:38:49 -0300

    Adicionado Metadata e nova forma de annotation

--- app/autoload.php
+++ app/autoload.php
@@ -12,6 +12,8 @@ $loader->registerNamespaces(array(
     'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
+    'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    
     'Acme'             => __DIR__.'/../src',
     'Mastop'           => __DIR__.'/../src',
 
