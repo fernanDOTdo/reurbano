2072846dafaf8828da37c45fddf3e65b53444cb6
Fernando Santos <o@fernan.do>
2011-06-10 03:14:08 -0300

    Novo esquema para atualização de Submodulos

--- src/Acme/DemoBundle/Controller/DemoController.php
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -4,47 +4,54 @@ namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Acme\DemoBundle\Form\ContactForm;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DemoController extends Controller
 {
     /**
-     * @Route("/", name="_demo")
-     * @Template()
-     */
+* @Route("/", name="_demo")
+* @Template()
+*/
     public function indexAction()
     {
         return array();
     }
 
     /**
-     * @Route("/hello/{name}", name="_demo_hello")
-     * @Template()
-     */
+* @Route("/hello/{name}", name="_demo_hello")
+* @Template()
+*/
     public function helloAction($name)
     {
         return array('name' => $name);
     }
 
     /**
-     * @Route("/contact", name="_demo_contact")
-     * @Template()
-     */
+* @Route("/contact", name="_demo_contact")
+* @Template()
+*/
     public function contactAction()
     {
-        $form = ContactForm::create($this->get('form.context'), 'contact');
+        $form = $this->get('form.factory')->create(new ContactType());
 
-        $form->bind($this->container->get('request'), $form);
-        if ($form->isValid()) {
-            $form->send($this->get('mailer'));
+        $request = $this->get('request');
+        if ('POST' == $request->getMethod()) {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                $mailer = $this->get('mailer');
+                // .. setup a message and send it
+                // http://symfony.com/doc/current/cookbook/email.html
 
-            $this->get('session')->setFlash('notice', 'Message sent!');
+                $this->get('session')->setFlash('notice', 'Message sent!');
 
-            return new RedirectResponse($this->generateUrl('_demo'));
+                return new RedirectResponse($this->generateUrl('_demo'));
+            }
         }
 
-        return array('form' => $form);
+        return array('form' => $form->createView());
     }
 }
