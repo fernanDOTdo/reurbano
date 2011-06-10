d0813f690653771d90dce00bcede66eac9ffaa9a
Fernando Santos <o@fernan.do>
2011-06-10 03:26:10 -0300

    Vendors

--- bin/vendors.sh
+++ bin/vendors.sh
@@ -8,8 +8,5 @@ BUNDLES=$VENDOR/bundles
 echo "> Atualizando Submódulos "
 git submodule update --init
 
-# Update the bootstrap files
-$DIR/bin/build_bootstrap.php
-
-# Update assets
-$DIR/app/console assets:install $DIR/web/
+# Update submodules
+$DIR/bin/vendors update
