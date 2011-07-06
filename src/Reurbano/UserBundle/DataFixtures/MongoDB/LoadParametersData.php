5f0950ab54ad6232923dadb6d93a1e0c9907b474
Fábio Egas <fabio@mastop.com.br>
2011-07-06 02:03:46 -0300

    Mais coisas

--- src/Reurbano/UserBundle/DataFixtures/MongoDB/LoadParametersData.php
+++ src/Reurbano/UserBundle/DataFixtures/MongoDB/LoadParametersData.php
@@ -23,29 +23,45 @@ class LoadParametersData implements FixtureInterface, ContainerAwareInterface {
         $param->setDesc('Configurações para usuários do site');
         $param->setBundle('user');
         $param->setOrder(1);
-        
+
+        $child = new Children();
+        $child->setName('allownew');
+        $child->setTitle('Permitir novos cadastros');
+        $child->setDesc('Permitir que novos usuários se cadastrem.');
+        $child->setValue(1);
+        $child->setFieldtype('checkbox');
+        $child->setOrder(0);
+        $param->addChildren($child);
+
         $child = new Children();
         $child->setName('mailnotify');
         $child->setTitle('Notificar Novos Cadastros');
-        $child->setDesc('Digite endereços de e-mail que serão notificados quando novos usuários se cadastrarem.');
+        $child->setDesc('Digite endereços de e-mail, separados por vírgula, que serão notificados quando novos usuários se cadastrarem.');
         $child->setValue('meu@email.com');
+        $child->setOrder(1);
         $param->addChildren($child);
+
         $child = new Children();
         $child->setName('autoactive');
         $child->setTitle('Ativação');
         $child->setDesc('Tipo de ativação de novos cadastros');
         $child->setValue('email');
         $child->setFieldtype('choice');
-        $child->setOpts(array('choices' => array('auto'   => 'Automático', 'email' => 'E-mail', 'admin'   => 'Aprovação Manual')));
+        $child->setOpts(array('choices' => array('auto' => 'Automático', 'email' => 'E-mail', 'admin' => 'Aprovação Manual')));
+        $child->setOrder(2);
         $param->addChildren($child);
+
         $child = new Children();
         $child->setName('selfdelete');
         $child->setTitle('Deletar Conta');
         $child->setDesc('Permitir que os usuários deletem o próprio cadastro.');
         $child->setValue('0');
         $child->setFieldtype('checkbox');
+        $child->setOrder(3);
         $param->addChildren($child);
+        
         $manager->persist($param);
         $manager->flush();
     }
+
 }
