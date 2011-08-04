a366809ccac7f2dc9c5d975efe913f68522571a8
Basquens <rafael@basquens.com>
2011-08-04 15:31:03 -0300

    teste

--- src/Reurbano/DealBundle/Controller/Frontend/SellController.php
+++ src/Reurbano/DealBundle/Controller/Frontend/SellController.php
@@ -4,6 +4,8 @@ namespace Reurbano\DealBundle\Controller\Frontend;
 use Mastop\SystemBundle\Controller\BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Reurbano\DealBundle\Document\Site;
+
 
 /**
  * Controller que cuidará das Ofertas em Frontend.
@@ -18,6 +20,40 @@ class SellController extends BaseController
      * @Template()
      */
     public function indexAction()
+    {
+        $title = "Venda cupons de qualquer site de compras coletivas aqui";
+        $form = $this->createFormBuilder()
+                ->add('site', 'text')
+                ->add('cupom', 'text')
+                ->getForm();
+        return array(
+            'title' => $title,
+            'form'  => $form->createView(),
+        );
+    }
+    
+    /**
+     * Ajax dos sites de compra coletiva.
+     * 
+     * @route("/ajax", name="deal_sell_ajax")
+     */
+    public function ajaxAction()
+    {
+        if ($this->get('request')->isXmlHttpRequest()) {
+            if ($this->get('request')->getMethod() == 'POST') {
+                
+            }
+        }
+        return array();
+    }
+    
+    /**
+     * Detalhes da oferta
+     * 
+     * @route("/detalhes", name="deal_sell_details")
+     * @Template()
+     */
+    public function detailsAction()
     {
         return array();
     }
