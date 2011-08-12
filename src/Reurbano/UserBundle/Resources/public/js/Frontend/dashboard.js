3ac6e0f2e346ce5211e20756a6cadd5a59b6239d
Basquens <rafael@basquens.com>
2011-08-12 12:16:34 -0300

    Para pegar as alterações do fábio

---
+++ src/Reurbano/UserBundle/Resources/public/js/Frontend/dashboard.js
@@ -0,0 +1,5 @@
+// perform JavaScript after the document is scriptable.
+$(function() {
+	// setup ul.tabs to work as tabs for each div directly under div.panes
+	$("ul.tabs").tabs("div.panes > div");
+});
